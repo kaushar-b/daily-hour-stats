cff4ac413a3d4b56b6752783de8aa309d5d8d16c
kaushar-b <kaushar5000@gmail.com>
2026-09-14 10:16:26 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+// Replace ALL values below with YOUR Firebase project values
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMMiNywjGhjaUKAH-HUmrkFdvVXCse6Ow",
+  authDomain: "daily-hour-stats.firebaseapp.com",
+  databaseURL: "https://daily-hour-stats-default-rtdb.firebaseio.com/",
+  projectId: "daily-hour-stats",
+  storageBucket: "daily-hour-stats.firebasestorage.app",
+  messagingSenderId: "578469883451",
+  appId: "1:578469883451:web:e8729b2bb16a189f87f61e"
+};
